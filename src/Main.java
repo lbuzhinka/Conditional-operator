382655128ce60a0c4440dd3a3cd36a7607a6cf42
lbuzhinka <lbuzhinka@gmail.com>
2024-04-09 10:45:40 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        System.out.println("Task 1");
+        int age = 17;
+        if (age >= 18)
+        {System.out.println("Вы совершеннолетний");}
+        else {System.out.println("Вы несовершеннолетний, нужно немного подождать");}
+        System.out.println("\nTask 2");
+
+
+    }}
